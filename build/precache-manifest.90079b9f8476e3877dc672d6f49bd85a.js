2eee6e019e2530953b0f5b6182fd225008645908
wdunn <williamdunn195@gmail.com>
2019-09-13 17:46:31 -0400

    add production build

---
+++ build/precache-manifest.90079b9f8476e3877dc672d6f49bd85a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e3871f045f67a42ce7b3d5c282bebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88a661e78f6efe523d72",
+    "url": "/static/css/main.df8f66ac.chunk.css"
+  },
+  {
+    "revision": "97fa0a5a4a9084ba0cde",
+    "url": "/static/js/2.e5ccd7db.chunk.js"
+  },
+  {
+    "revision": "88a661e78f6efe523d72",
+    "url": "/static/js/main.da0f8d45.chunk.js"
+  },
+  {
+    "revision": "3d49b73091e9530fe297",
+    "url": "/static/js/runtime~main.93c4184f.js"
+  }
+]);
